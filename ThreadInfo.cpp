86bb3ba29dbe09843c2735907b71b328492b184e
CyberConductor <gilad4025@gmail.com>
2026-02-25 00:36:36 +0200

    added threads proccesses

---
+++ ThreadInfo.cpp
@@ -0,0 +1,30 @@
+#include "ThreadInfo.h"
+#include <vector>
+#include <TlHelp32.h>
+#include <Psapi.h>
+std::vector <ThreadInfo> getThreadsProcesses(DWORD pid)
+{
+	std::vector<ThreadInfo> threads;
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD,0);
+	if (snapshot == INVALID_HANDLE_VALUE)
+		return threads;
+	THREADENTRY32 entry;
+	entry.dwSize = sizeof(THREADENTRY32);
+	if (Thread32First(snapshot, &entry))
+	{
+		do
+		{
+			if (entry.th32OwnerProcessID = pid)
+			{
+				ThreadInfo info;
+				info.threadID = entry.th32ThreadID;
+				info.ownerPID = entry.th32OwnerProcessID;
+				threads.push_back(info);
+			}
+
+		} while (Thread32Next(snapshot, &entry));
+	}
+
+	CloseHandle(snapshot);
+	return threads;
+}
---
+++ ThreadInfo.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <windows.h>
+struct ThreadInfo
+{
+	DWORD threadID;
+	DWORD ownerPID;
+};
