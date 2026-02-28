e7b2e10f6cd6f410533ca3d699c2697ce744fd09
CyberConductor <gilad4025@gmail.com>
2026-03-01 00:41:17 +0200

    fixed function signature

--- RegDetect.h
+++ RegDetect.h
@@ -2,5 +2,5 @@
 #include <windows.h>
 #include <string>
 
-void CheckStartupRegistry(HKEY rootkey, const std::wstring& subKey);
+void CheckStartupRegistry(HKEY rootKey, const std::wstring& subKey);
 void ScanStartupKeys();
